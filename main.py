02f82df54379f7d995336230e089d03acdc43b13
VikVal <vvalujkina@gmail.com>
2024-01-08 13:24:24 +0300

    HW6

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+import uvicorn
+
+from routers import user, product, order
+from db import database
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+
+app.include_router(user.router, tags=['users'])
+app.include_router(product.router, tags=['products'])
+app.include_router(order.router, tags=['orders'])
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=80, reload=True)
